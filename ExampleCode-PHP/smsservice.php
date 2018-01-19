70be17ef7de8161187802ddd78e58af3bd62a9b9
chakkapoom <chakkapoom@gmail.com>
2018-01-19 10:24:52 +0700

    checkpoint add sms std.

--- ExampleCode-PHP/smsservice.php
+++ ExampleCode-PHP/smsservice.php
@@ -2,17 +2,20 @@
 	include("sms.class.test.php");
 	$username = "xxxx";
 	$password = "thisispassword";
-	$msisdn = $_REQUEST['msisdn'];
+	$msisdn = $_REQUEST['number'];
 	$message = $_REQUEST['message'];
 	$sender = "SMS";
 	$ScheduledDelivery = $_REQUEST['ScheduledDelivery'];
-	$force = $_REQUEST['force'];
+	$force = "premium";
+	// $force = "standard";
 	
 	$result = sms::send_sms($username,$password,$msisdn,$message,$sender,$ScheduledDelivery,$force);
-	echo "<pre>";
-	print_r($result);
-	echo "</pre>";
-
-	// echo json_encode($result);
+	
+	// echo "<pre>";
+	// print_r($result);
+	// echo "</pre>";
+	
+	// echo "<pre>",json_encode($result, JSON_PRETTY_PRINT),"</pre>";
+	echo json_encode($result);
 
 ?>
