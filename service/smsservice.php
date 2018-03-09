edb18b30258ab43eec452271cc33f8aea26a7429
chakkapoom7 <chakkapoom@gmail.com>
2018-03-09 17:01:44 +0700

    format some code

--- service/smsservice.php
+++ service/smsservice.php
@@ -10,7 +10,6 @@ ini_set('display_startup_errors', true);
 
 include "sms.class.test.php";
 include "get_info.class.php";
-include_once "connect/connect.php";
 
 // $provider_username = "0873578044";
 // $provider_password = "956983";
@@ -30,36 +29,13 @@ $lang = "th";
 
 
 
+
 //=================================================================================================
+//----------------------------------------- get sms user data. ------------------------------------
 $user_info = get_info::get_user_info($user_sender,$pass_sender);
 $current_usage = @$user_info['current_usage'];
 $maximum_quota = @$user_info['maximum_quota'];
 $status = @$user_info['status'];
-//=================================================================================================
-
-
-//=================================================================================================
-//reserve for quota calculate.
-if (!preg_match('/[^A-Za-z0-9]/', $message)) // '/[^a-z\d]/i' should also work.
-{
-    // string contains only english letters & digits
-    $lang="en";
-}
-
-// $credit_result = sms::check_credit($provider_username,$provider_password,$forceremain);
-$credit_result = sms::check_credit("0873578044","956983",$forceremain);
-
-
-$user_table = get_info::get_group_info(@$_REQUEST['group']);
-
-
-$message_lent = strlen($message);
-
-// get_info::return_result("message lent : ".$message_lent);
-
-$credit_use=0;
-//=================================================================================================
-
 
 if ($status != "active") {
     get_info::return_result("บัญชีของคุณไม่มีสิทธิ์ใช้งาน โปรดติดต่อทีม IT");
@@ -68,18 +44,50 @@ if ($status != "active") {
 if ($current_usage + $credit_use > $maximum_quota) {
     get_info::return_result("คุณมีเครดิต ไม่เพียงพอสำหรับการส่ง");
 }
+//=================================================================================================
 
 
-$type = @$_REQUEST['type'];
+
+
+//=================================================================================================
+//------------------------------- reserve for quota calculate. ------------------------------------
+
+// $credit_result = sms::check_credit($provider_username,$provider_password,$forceremain);
+$credit_result = sms::check_credit("0873578044","956983",$forceremain);
+//=================================================================================================
+
+
+
+
+
+//=================================================================================================
+//----------------------------------- get user destination. ---------------------------------------
+
+$user_table = get_info::get_group_info(@$_REQUEST['group']);
+//=================================================================================================
+
+
+
+
+//=================================================================================================
+//------------------------------------- call credit to use. ---------------------------------------
+$message_lent = strlen($message);
+// get_info::return_result("message lent : ".$message_lent);
+if (!preg_match('/[^A-Za-z0-9]/', $message)) // '/[^a-z\d]/i' should also work.
+{
+    // string contains only english letters & digits
+    $lang="en";
+}
+$credit_use=0;
+//=================================================================================================
+
+
 
 $result = sms::send_sms($provider_username, $provider_password, $msisdn, $message, $sender, $ScheduledDelivery, $force);
 
 
-// // echo "<pre>";
-// // print_r($result);
-// // echo "</pre>";
 
-// echo "<pre>",json_encode($result, JSON_PRETTY_PRINT),"</pre>";
+$type = @$_REQUEST['type'];
 
 $last_resault = array_merge($credit_result,$result);
 
